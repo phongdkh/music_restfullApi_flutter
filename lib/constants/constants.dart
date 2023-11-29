34592cf455ef79353e5c53fc75d7289e0eb65638
Hoai Phong <hoaiphonghp@gmail.com>
2023-11-29 14:30:07 +0700

    update

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class Constants {
   static String baseURL = "https://storage.googleapis.com/uamp/";
   static String KEY = "9c31c9bb55f379db5988e9ea16a77cff";
-  static const String versionAPI = "catalog.json";
+  static const String versionAPI = "/api/v1";
   static const String versionPath = "/api/v1";
   static const String v2Path = "/api/v2";
   static const String defaultUrlWebView = "https://";
